e12a92384e7f0c97b84acc4e019df7a87546eb9a
FixingWoo <fixingwoo@gmail.com>
2025-07-09 17:20:52 +0900

    refactor: 디렉토리 구조

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,15 @@
+// 기본 컴포넌트들
 export * from './Button';
 export * from './Column';
 export * from './ElementType';
 export * from './Flex';
 export * from './Heading';
 export * from './Header';
-export * from './Icon/index';
+export * from './Icon';
 export * from './ToggleButton';
 export * from './Text';
 export * from './ThemeToggle';
+
+// 하위 디렉토리 컴포넌트들
+export * from './cards';
+export * from './lists';
